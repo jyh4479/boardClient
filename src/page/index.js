fac69a67e029e61b738457a017cf177e8503e2d3
Jeong yong hoon <jyh.dev@gmail.com>
2022-01-24 22:59:10 +0900

    UPDATE COMPONENT LINK

---
+++ src/page/index.js
@@ -0,0 +1,4 @@
+export {default as HomePage} from './HomePage'
+export {default as LoginPage} from './LoginPage'
+export {default as CreateContentPage} from './CreateContentPage'
+export {default as DetailPage} from './DetailPage'
